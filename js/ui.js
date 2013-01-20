3025aac32bd225506c98e20f4b01283eef1cd159
jhalak <jhalak1983@yahoo.com>
2013-01-20 16:26:39 +0600

    Done add user functionality

--- js/ui.js
+++ js/ui.js
@@ -8,16 +8,17 @@
 $(function() {
     // there's the talent and the project
     var $talent = $( ".talent" ),
-      $project = $( ".project" );
+      $project = $( ".project" ),
+      $draggableOptions = {
+          cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+          revert: "invalid", // when not dropped, the item will revert back to its initial position
+          containment: "document",
+          helper: "clone",
+          cursor: "move"
+        };
 
     // let the talent items be draggable
-    $( "li", $talent ).draggable({
-      cancel: "a.ui-icon", // clicking an icon won't initiate dragging
-      revert: "invalid", // when not dropped, the item will revert back to its initial position
-      containment: "document",
-      helper: "clone",
-      cursor: "move"
-    });
+    $( "li", $talent ).draggable($draggableOptions);
     
     attachBdlClick($(".talent li, .project li"));
     
@@ -86,6 +87,47 @@ $(function() {
         attachBdlClick(clone);
       });
     }
+    
+    // add talent form
+    $("#talent_add_form").dialog({
+      autoOpen : false,
+      height : 300,
+      width : 350,
+      modal : true,
+      buttons : {
+        "Add talent" : function() {
+          var bValid = true,
+          $talentName = $("#talent_name").val(),
+          $talentId = 50; // TODO: need to set it dynamically
+          var $newTalent = $(
+            '<li id="talent' + $talentId + '" name="' + $talentName + '" tid="' + $talentId + '" class="ui-widget-content ui-corner-tr">' + 
+              '<h5 class="ui-widget-header">' + $talentName + '</h5>' + 
+            '</li>')
+          .draggable($draggableOptions);
+          attachBdlClick($newTalent);
+          if (bValid) {
+            $($talent).append($newTalent);
+            $(this).dialog("close");
+          }
+        },
+        Cancel : function() {
+          $(this).dialog("close");
+        }
+      },
+      close : function() {
+        allFields.val("").removeClass("ui-state-error");
+      }
+    });
+    
+    // add onclick event in "Add talent" button
+    $( "#btn_add_talent" ).button().click(function() {
+      $( "#talent_add_form" ).dialog("open");
+    });
+    
+    // add onclick event in "Generate report" button
+    $( "#btn_gen_report" ).button().click(function() {
+      ReportController();
+    });
     
     
     
